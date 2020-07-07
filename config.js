180464d1ef0c9dd857eea8763b0827184f88d64b
Blaine10 <67975117+Blaine10@users.noreply.github.com>
2020-07-08 02:24:09 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'fCOhfQDOx3V6aap0QnHy8phfm',
+  consumer_secret: 'd0tHmjYnqrsZFJ0hSXtk3WAgefVz4mmWZPgyhvHpSAjCoZflbC',
+  access_token_key: '1278760556988489728-dzojKNJ2kgHIJEw4R68YD4RvjkwwEx',
+  access_token_secret: '2NlHoPTeQZ0okmwOZggf2HeUWYym1Mx4bKHSRDus7lVwv'
+}
